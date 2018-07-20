cb35bab63cf14e082b0a1ca504dc979573981025
Philip Wright <philip.bigbang@gmail.com>
2018-07-19 17:17:57 -0700

    Fixed a bug in the distance function.

--- Easy.cs
+++ Easy.cs
@@ -6,7 +6,7 @@ namespace Easy
     {
         public static Random Random = new Random();
 
-        public static double Distance(double x1, double y1, double x2, double y2)
+        public static double Distance(double x1, double x2, double y1, double y2)
         {
             return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
         }
